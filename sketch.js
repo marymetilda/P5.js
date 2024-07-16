78bd0bda9ffad155440b3ecf4f5f3feb9141ccc8
mary-metilda <metildaphy@gmail.com>
2024-07-16 15:31:58 +0530

    2.feat: circle growing animation

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
+let circleX;
+let circleRadius;
+
 function setup() {
   createCanvas(400, 400);
   background(0);
+  circleX = 0;
+  circleRadius = 24;
 }
 
 function draw() {
+  background(0);
   noStroke();
-  fill(0, 255, 250, 50);
-  circle(mouseX + 50, mouseY - 50, 8);
+  fill(255, 200);
+  circle(circleX, 50, circleRadius);
 
-  noStroke();
-  fill(255, 0, 250, 50);
-  circle(mouseX + 20, mouseY - 20, 8);
-
-  noStroke();
-  fill(255, 250, 0, 50);
-  circle(mouseX + 70, mouseY - 70, 8);
+  circleX = circleX + 1;
+  circleRadius = circleRadius + 1;
 }
 
 function mousePressed() {
   background(0);
+  circleX = 0;
+  circleRadius = 24;
 }
