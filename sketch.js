5cf94f924d5f764031fb78034da54837c6d0ec5c
mary-metilda <metildaphy@gmail.com>
2024-07-16 15:14:48 +0530

    feat: moving the dot with mouse

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup() {
   createCanvas(400, 400);
+  background(0);
 }
 
 function draw() {
-  background(220, 100, 100);
+  noStroke();
+  fill(0, 255, 250, 50);
+  circle(mouseX + 50, mouseY - 50, 8);
+
+  noStroke();
+  fill(255, 0, 250, 50);
+  circle(mouseX + 20, mouseY - 20, 8);
+
+  noStroke();
+  fill(255, 250, 0, 50);
+  circle(mouseX + 70, mouseY - 70, 8);
+}
+
+function mousePressed() {
+  background(0);
 }
