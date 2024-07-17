505ced642e796c5d8e55b636d3f28300d7aebec0
mary-metilda <metildaphy@gmail.com>
2024-07-17 14:12:21 +0530

    24.feat: create bubbles on click

---
+++ 24/bubble.js
@@ -0,0 +1,17 @@
+class Bubble {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+  move() {
+    this.x += random(-5, 5);
+    this.y += random(-5, 5);
+  }
+
+  show() {
+    noStroke();
+    fill(255, 30);
+    ellipse(this.x, this.y, this.r * 2);
+  }
+}
