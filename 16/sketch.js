f0fc5cf0f5d5d3a66c9fbf1feec3ff2ee10539d7
mary-metilda <metildaphy@gmail.com>
2024-07-17 11:13:21 +0530

    16.feat: moving colored circles

---
+++ 16/sketch.js
@@ -0,0 +1,18 @@
+var offset = 0;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  strokeWeight(4);
+  stroke(255);
+
+  for (var x = 0; x <= width; x += 50) {
+    fill(0, random(255), random(255));
+    ellipse(x + offset, 200, 25, 25);
+  }
+
+  offset++;
+}
